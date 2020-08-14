8c964cb73d868f33d6c4cae2012077b4fd72a7fa
adrienLamoureux <vergil1534@gmail.com>
2020-08-14 12:15:08 +0200

    Fix DynamoDB config setup (dev only)

--- src/databases/dynamodb/init.go
+++ src/databases/dynamodb/init.go
@@ -1,15 +1,24 @@
 package dynamodb
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 var svc *dynamodb.DynamoDB
 
+var (
+	region   = "eu-central-1"
+	endpoint = "http://localhost:8000"
+)
+
 func init() {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
+		Config: aws.Config{
+			Region:   &region,
+			Endpoint: &endpoint,
+		},
 	}))
 
 	// Create DynamoDB client
